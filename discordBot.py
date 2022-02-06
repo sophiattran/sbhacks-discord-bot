c53817dee897ab0d46d563ba59991cc4b33601b9
Riley Chou <riley33chou@gmail.com>
2022-02-05 21:31:32 -0800

    dog pic feature

--- discordBot.py
+++ discordBot.py
@@ -1,8 +1,10 @@
 import discord
 import os
+import requests
 
 client = discord.Client()
 
+token = '' #insert token here!!
 
 @client.event
 async def on_ready():
@@ -18,4 +20,14 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+# dog image feature
+    if message.content.startswith('$dogpics'):
+        dog_info = requests.get("https://dog.ceo/api/breeds/image/random")
+        embed = discord.Embed(
+            title="Dog!", description="Feeling down? Have a dog!", color=0x047C91
+        )
+        embed.set_image(url=dog_info.json()["message"])
+        await message.channel.send(embed=embed)
+        
+
+client.run(token)
